29313cf19f05b63f186b505b5a7426ed07109107
Josquin2 <Josquin2@mail.ru>
2024-03-02 14:17:36 +0300

    updated negative numbers

--- simple-calculator/script/index.js
+++ simple-calculator/script/index.js
@@ -2,15 +2,16 @@ let first = '';
 
 function inputValue(value){
     let currentVal = document.getElementById('screen').value;
-    if (value === '*'||
+    if ((currentVal != '') &&
+        (value === '*'||
         value === '/'||
         value === '+'||
-        value === '-'
+        value === '-')
     ){
         updateInt(currentVal, value)
     }
     else{
-        originalColors();
+        originalColors();  
         
         if (value == '.'){
             if (!(currentVal.toString().includes('.')) && (currentVal.toString().length > 0)){
@@ -42,19 +43,19 @@ function clearScreen(){
 }
 
 function percent(){
-    let ans = document.getElementById('screen').value;
-    ans = ans + '/100';
-    if (ans.toString().length > 0){
-        let result = eval(ans);
+    let answer = document.getElementById('screen').value;
+    answer = answer + '/100';
+    if (answer.toString().length > 0){
+        let result = eval(answer);
         document.getElementById('screen').value = result;
     }
-    
+  
 }
 function calculation(first){
     let screen = document.getElementById('screen').value;
-    let vir = first + screen;
-    if (eval(vir) != undefined){
-        document.getElementById('screen').value = eval(vir);
+    let both = first + screen;
+    if (eval(both) != undefined){
+        document.getElementById('screen').value = eval(both);
     }
     else {
         document.getElementById('screen').value = 'Error!'
@@ -63,24 +64,24 @@ function calculation(first){
 }
 
 
-function updateInt(firstValue, znak){
-    let alo = first.toString()[first.toString().length - 1]
-    if (alo == '/'||
-        alo == '*'||
-        alo == '+'||
-        alo == '-'
+function updateInt(firstValue, sign){
+    let lastsign = first.toString()[first.toString().length - 1]
+    if (lastsign == '/'||
+        lastsign == '*'||
+        lastsign == '+'||
+        lastsign == '-'
     ){
         firstValue = first.slice(0, first.length - 1);
-        firstValue += document.getElementById(znak).value;
+        firstValue += document.getElementById(sign).value;
         originalColors();
-        document.getElementById(znak).classList.add('pressed');
+        document.getElementById(sign).classList.add('pressed');
         document.getElementById('screen').value = '';
         return first = firstValue;
     }
     else{
         originalColors();
-        document.getElementById(znak).classList.add('pressed');
-        firstValue += document.getElementById(znak).value;
+        document.getElementById(sign).classList.add('pressed');
+        firstValue += document.getElementById(sign).value;
         document.getElementById('screen').value = '';
         return first = firstValue;
     }
