595818f29818063713d631313713fe4b2723bf04
Josquin2 <Josquin2@mail.ru>
2024-03-12 16:36:22 +0300

    last commit

--- simple-calculator/script/index.js
+++ simple-calculator/script/index.js
@@ -1,97 +1,89 @@
-let first = '';
+let first = "";
 
-function inputValue(value){
-    let currentVal = document.getElementById('screen').value;
-    if ((currentVal != '') &&
-        (value === '*'||
-        value === '/'||
-        value === '+'||
-        value === '-')
-    ){
-        updateInt(currentVal, value)
-    }
-    else{
-        originalColors();  
-        
-        if (value == '.'){
-            if (!(currentVal.toString().includes('.')) && (currentVal.toString().length > 0)){
-                document.getElementById('screen').value += value;
-            }
-        }
-        else{
-            document.getElementById('screen').value += value;
-        }
-    }
-    
-}
-
-function deleteLastElement(){
-    let value = document.getElementById('screen').value;
-    if (value.length == 1){
-        document.getElementById('screen').value = '';
-    }
-    else{
-        document.getElementById('screen').value = value.toString().slice(0, value.length - 1)
-    }
-}
-
-function clearScreen(){
-    document.getElementById('screen').value = '';
+function inputValue(value) {
+  let currentVal = document.getElementById("screen").value;
+  if (
+    currentVal != "" &&
+    (value === "*" || value === "/" || value === "+" || value === "-")
+  ) {
+    updateInt(currentVal, value);
+  } else {
     originalColors();
-    return first = '';
 
+    if (value == ".") {
+      if (
+        !currentVal.toString().includes(".") &&
+        currentVal.toString().length > 0
+      ) {
+        document.getElementById("screen").value += value;
+      }
+    } else {
+      document.getElementById("screen").value += value;
+    }
+  }
 }
 
-function percent(){
-    let answer = document.getElementById('screen').value;
-    answer = answer + '/100';
-    if (answer.toString().length > 0){
-        let result = eval(answer);
-        document.getElementById('screen').value = result;
-    }
-  
-}
-function calculation(first){
-    let screen = document.getElementById('screen').value;
-    let both = first + screen;
-    if (eval(both) != undefined){
-        document.getElementById('screen').value = eval(both);
-    }
-    else {
-        document.getElementById('screen').value = 'Error!'
-    }
-    
+function deleteLastElement() {
+  let value = document.getElementById("screen").value;
+  if (value.length == 1) {
+    document.getElementById("screen").value = "";
+  } else {
+    document.getElementById("screen").value = value
+      .toString()
+      .slice(0, value.length - 1);
+  }
 }
 
-
-function updateInt(firstValue, sign){
-    let lastsign = first.toString()[first.toString().length - 1]
-    if (lastsign == '/'||
-        lastsign == '*'||
-        lastsign == '+'||
-        lastsign == '-'
-    ){
-        firstValue = first.slice(0, first.length - 1);
-        firstValue += document.getElementById(sign).value;
-        originalColors();
-        document.getElementById(sign).classList.add('pressed');
-        document.getElementById('screen').value = '';
-        return first = firstValue;
-    }
-    else{
-        originalColors();
-        document.getElementById(sign).classList.add('pressed');
-        firstValue += document.getElementById(sign).value;
-        document.getElementById('screen').value = '';
-        return first = firstValue;
-    }
-    
-    
+function clearScreen() {
+  document.getElementById("screen").value = "";
+  originalColors();
+  return (first = "");
 }
 
-function originalColors(){
-    document.getElementById('/').classList = 'cals';
-    document.getElementById('*').classList = 'cals';
-    document.getElementById('+').classList = 'cals';
-    document.getElementById('-').classList = 'cals';
+function percent() {
+  let answer = document.getElementById("screen").value;
+  answer = answer + "/100";
+  if (answer.toString().length > 0) {
+    let result = eval(answer);
+    document.getElementById("screen").value = result;
+  }
+}
+function calculation(first) {
+  let screen = document.getElementById("screen").value;
+  let both = first + screen;
+  if (eval(both) != undefined) {
+    document.getElementById("screen").value = eval(both);
+  } else {
+    document.getElementById("screen").value = "Error!";
+  }
+}
+
+function updateInt(firstValue, sign) {
+  let lastsign = first.toString()[first.toString().length - 1];
+  if (
+    lastsign == "/" ||
+    lastsign == "*" ||
+    lastsign == "+" ||
+    lastsign == "-"
+  ) {
+    firstValue = first.slice(0, first.length - 1);
+    firstValue += document.getElementById(sign).value;
+    originalColors();
+    document.getElementById(sign).classList.add("pressed");
+    document.getElementById("screen").value = "";
+    return (first = firstValue);
+  } else {
+    originalColors();
+    document.getElementById(sign).classList.add("pressed");
+    firstValue += document.getElementById(sign).value;
+    document.getElementById("screen").value = "";
+    return (first = firstValue);
+  }
+}
+
+function originalColors() {
+  document.getElementById("/").classList = "cals";
+  document.getElementById("*").classList = "cals";
+  document.getElementById("+").classList = "cals";
+  document.getElementById("-").classList = "cals";
 }
